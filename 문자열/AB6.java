056db5bdd1708d81fb9b12e3659dd6145c5feb94
UmJaeJeong <umjaejeong@gmail.com>
2022-01-21 11:34:59 +0900

    A+B+6 알고리즘 수정 커밋

--- 문자열/AB6.java
+++ 문자열/AB6.java
@@ -6,13 +6,11 @@ public class AB6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		sc.nextLine();
 		for(int i=0; i<n;i++) {
 			String str = sc.nextLine();
-			System.out.println(str);
 			System.out.println(str.charAt(0)-'0'+str.charAt(2)-'0');
 		}
-		
-
 	}
 
 }
